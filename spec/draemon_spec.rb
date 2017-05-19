4220fae082fa123b102213dae22ca25e67975e26
Lukas Nimmo <Lukas.nimmo@gmail.com>
2017-05-19 13:29:00 -0500

    Changed 'have' -> 'execute' in test name.

--- spec/draemon_spec.rb
+++ spec/draemon_spec.rb
@@ -7,7 +7,7 @@ describe Draemon do
     expect(Draemon::VERSION).not_to be nil
   end
 
-  it 'cannot have more than one command' do
+  it 'cannot execute more than one command' do
     expect{ draemon.start.stop.daemonize! }.to raise_error(ArgumentError)
   end
 end
