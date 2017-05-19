33987c702b977ce27fed04df6202452fccbd2df9
Lukas Nimmo <Lukas.nimmo@gmail.com>
2017-05-19 12:42:37 -0500

    Check for more than one command.

--- lib/draemon.rb
+++ lib/draemon.rb
@@ -1,5 +1,41 @@
 require 'draemon/version'
 
 class Draemon
+  COMMANDS = %w(--start --stop --status --help --version).freeze
 
+  def initialize
+    @execution_str = 'start-stop-daemon'
+  end
+
+  def start
+    @execution_str += ' --start '
+    self
+  end
+
+  def stop
+    @execution_str += ' --stop '
+    self
+  end
+
+  def daemonize!
+    check_errors
+    `#{@execution_str}`
+  end
+
+  private
+
+  def check_errors
+    only_one_command
+  end
+
+  def only_one_command
+    # Possible more descriptive errors?
+    # Otherwise everything will be an argument error...
+    command_count = 0
+    COMMANDS.each do |command|
+      command_count += 1 if @execution_str.include? command
+    end
+
+    raise ArgumentError, 'You can only have one command.' if command_count > 1
+  end
 end
--- spec/draemon_spec.rb
+++ spec/draemon_spec.rb
@@ -1,7 +1,13 @@
 require 'spec_helper'
 
 describe Draemon do
+  let(:draemon) { Draemon.new }
+
   it 'has a version number' do
     expect(Draemon::VERSION).not_to be nil
+  end
+
+  it 'cannot have more than one command' do
+    expect{ draemon.start.stop.daemonize! }.to raise_error(ArgumentError)
   end
 end
