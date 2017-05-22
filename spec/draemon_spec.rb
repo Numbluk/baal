2a270860ebb0a70931beed93206c45ed66280ca6
Lukas Nimmo <Lukas.nimmo@gmail.com>
2017-05-22 13:17:08 -0500

    Added testing for building of execution string with commands.

--- spec/draemon_spec.rb
+++ spec/draemon_spec.rb
@@ -36,4 +36,38 @@ describe Draemon do
       expect { draemon.start.daemonize! }.to raise_error(ArgumentError)
     end
   end
+
+  # TODO: test the execution string
+
+  context 'when building the execution string using a command' do
+    let(:draemon) { Draemon.new }
+
+    it '#start builds the correct execution string' do
+      draemon.start
+      expect(draemon.execution_str).to eq('start-stop-daemon --start')
+    end
+
+    it '#stop builds the correct execution string' do
+      draemon.stop
+      expect(draemon.execution_str).to eq('start-stop-daemon --stop')
+    end
+
+    it '#status builds the correct execution string' do
+      draemon.status
+      expect(draemon.execution_str).to eq('start-stop-daemon --status')
+    end
+
+    it '#help builds the correct execution string' do
+      draemon.help
+      expect(draemon.execution_str).to eq('start-stop-daemon --help')
+    end
+
+    it '#version builds the correct execution string' do
+      draemon.version
+      expect(draemon.execution_str).to eq('start-stop-daemon --version')
+    end
+  end
+
+  context 'matching options create the correct output sting'
+  context 'optional options create the correct output string'
 end
