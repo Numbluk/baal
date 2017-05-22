150476421eac83c85c02979034bf6e349ba54821
Lukas Nimmo <Lukas.nimmo@gmail.com>
2017-05-22 17:20:31 -0500

    Refactored optional options into its own module and separated tests from the Baal module.

--- lib/baal/optional_options.rb
+++ lib/baal/optional_options.rb
@@ -1,5 +1,147 @@
 module Baal
   module OptionalOptions
+    OPTIONAL_OPTS = {
+        group: '--group',
+        signal: '--signal',
+        retry: '--retry',
+        start_as: '--startas',
+        test: '--test',
+        oknodo: '--oknodo',
+        quiet: '--quiet',
+        chuid: '--chuid',
+        chroot: '--chroot',
+        chdir: '--chdir',
+        background: '--background',
+        no_close: '--no-close',
+        nice_level: '--nicelevel',
+        proc_sched: '--procsched',
+        io_sched: '--iosched',
+        umask: '--umask',
+        make_pid_file: '--make-pidfile',
+        remove_pid_file: '--remove-pidfile',
+        verbose: '--verbose'
+    }.freeze
 
+    def group(group_name_or_gid)
+      @execution_str += "#{OPTIONAL_OPTS[:group]}=#{group_name_or_gid}"
+      self
+    end
+    alias group_name group
+    alias gid group
+    alias change_to_group group
+    alias change_to_group_name group
+    alias change_to_gid group
+
+    def signal(signal = 'TERM')
+      @execution_str += "#{OPTIONAL_OPTS[:signal]}=#{signal}"
+      self
+    end
+    alias with_signal signal
+
+    def retry(seconds_or_schedule)
+      @execution_str += "#{OPTIONAL_OPTS[:retry]}=#{seconds_or_schedule}"
+      self
+    end
+    alias retry_timeout retry
+    alias retry_schedule retry
+
+    def start_as(path)
+      @execution_str += "#{OPTIONAL_OPTS[:start_as]}=#{path}"
+      self
+    end
+    alias startas start_as
+    alias start_at start_as
+
+    def test
+      @execution_str += "#{OPTIONAL_OPTS[:test]}"
+      self
+    end
+
+    def oknodo
+      @execution_str += "#{OPTIONAL_OPTS[:oknodo]}"
+      self
+    end
+
+    def quiet
+      @execution_str += "#{OPTIONAL_OPTS[:quiet]}"
+      self
+    end
+
+    def chuid(username_or_uid, group_or_gid = nil)
+      group_or_gid = group_or_gid.nil? ? '' : ":#{group_or_gid}"
+      @execution_str += "#{OPTIONAL_OPTS[:chuid]}=#{username_or_uid}#{group_or_gid}"
+      self
+    end
+    alias change_to_user chuid
+    alias change_to_uid chuid
+
+    def chroot(new_root_dir)
+      @execution_str += "#{OPTIONAL_OPTS[:chroot]}=#{new_root_dir}"
+      self
+    end
+
+    def chdir(path)
+      @execution_str += "#{OPTIONAL_OPTS[:chdir]}=#{path}"
+      self
+    end
+
+    def background
+      @execution_str += "#{OPTIONAL_OPTS[:background]}"
+      self
+    end
+
+    # Only relevant when using --background
+    def no_close
+      @execution_str += "#{OPTIONAL_OPTS[:no_close]}"
+      self
+    end
+
+    def nice_level(incr)
+      @execution_str += "#{OPTIONAL_OPTS[:nice_level]}=#{incr}"
+      self
+    end
+    alias incr_nice_level nice_level
+
+    # TODO: error checking for classes and priority
+    # TODO: allow symbols and underscores
+    def proc_sched(policy, priority = nil)
+      priority = priority.nil? ? ' ' : ":#{priority}"
+      @execution_str += "#{OPTIONAL_OPTS[:proc_sched]}=#{policy}#{priority}"
+      self
+    end
+    alias procshed proc_sched
+    alias process_schedule proc_sched
+
+    # TODO: error checking for classes and priority
+    # TODO: allow symbols and underscores
+    def io_sched(sched_class, priority)
+      priority = priority.nil? ? ' ' : ":#{priority}"
+      @execution_str += "#{OPTIONAL_OPTS[:io_sched]}=#{sched_class}#{priority}"
+      self
+    end
+    alias iosched io_sched
+    alias io_schedule io_sched
+
+    def umask(mask)
+      @execution_str += "#{OPTIONAL_OPTS[:umask]}=#{mask}"
+      self
+    end
+
+    def make_pid_file(pidfile_path)
+      @execution_str += "#{OPTIONAL_OPTS[:make_pid_file]}=#{pidfile_path}"
+      self
+    end
+    alias make_pidfile make_pid_file
+
+    def remove_pid_file(pidfile_path)
+      @execution_str += "#{OPTIONAL_OPTS[:remove_pid_file]}=#{pidfile_path}"
+      self
+    end
+    alias remove_pidfile remove_pid_file
+
+    def verbose
+      @execution_str += OPTIONAL_OPTS[:verbose]
+      self
+    end
   end
 end
---
+++ spec/optional_options_spec.rb
@@ -0,0 +1,117 @@
+require 'rspec'
+require 'baal/optional_options'
+
+class DummyDaemon
+  include Baal::OptionalOptions
+
+  attr_reader :execution_str
+
+  def initialize
+    @execution_str = ''
+  end
+end
+
+describe Baal::OptionalOptions do
+  context 'when building the execution string' do
+    let(:daemon) { DummyDaemon.new }
+
+    it '#group builds the correct execution string' do
+      daemon.group('public')
+      expect(daemon.execution_str).to eq('--group=public')
+    end
+
+    it '#signal builds the correct execution string' do
+      daemon.signal('SIGINT')
+      expect(daemon.execution_str).to eq('--signal=SIGINT')
+    end
+
+    it '#retry builds the correct execution string' do
+      daemon.retry(5)
+      expect(daemon.execution_str).to eq('--retry=5')
+    end
+
+    it '#start_as builds the correct execution string' do
+      start_as_path = '/start/as/path'
+      daemon.start_as(start_as_path)
+      expect(daemon.execution_str).to eq("--startas=#{start_as_path}")
+    end
+
+    it '#test builds the correct execution string' do
+      daemon.test
+      expect(daemon.execution_str).to eq('--test')
+    end
+
+    it '#oknodo builds the correct execution string' do
+      daemon.oknodo
+      expect(daemon.execution_str).to eq('--oknodo')
+    end
+
+    it '#quiet builds the correct execution string' do
+      daemon.quiet
+      expect(daemon.execution_str).to eq('--quiet')
+    end
+
+    it '#chuid builds the correct execution string' do
+      daemon.chuid(34)
+      expect(daemon.execution_str).to eq('--chuid=34')
+    end
+
+    it '#chroot builds the correct execution string' do
+      daemon.chroot('/bin')
+      expect(daemon.execution_str).to eq('--chroot=/bin')
+    end
+
+    it '#chdir builds the correct execution string' do
+      chdir_path = '../new/path'
+      daemon.chdir(chdir_path)
+      expect(daemon.execution_str).to eq("--chdir=#{chdir_path}")
+    end
+
+    it '#background builds the correct execution string' do
+      daemon.background
+      expect(daemon.execution_str).to eq('--background')
+    end
+
+    it '#no_close builds the correct execution string' do
+      daemon.no_close
+      expect(daemon.execution_str).to eq('--no-close')
+    end
+
+    it '#nice_level builds the correct execution string' do
+      daemon.nice_level(3)
+      expect(daemon.execution_str).to eq('--nicelevel=3')
+    end
+
+    it '#proc_sched builds the correct execution string' do
+      daemon.proc_sched('fifo', 2)
+      expect(daemon.execution_str).to eq('--procsched=fifo:2')
+    end
+
+    it '#io_sched builds the correct execution string' do
+      daemon.io_sched('best-effort', 20)
+      expect(daemon.execution_str).to eq('--iosched=best-effort:20')
+    end
+
+    it '#umask builds the correct execution string' do
+      daemon.umask(777)
+      expect(daemon.execution_str).to eq('--umask=777')
+    end
+
+    it '#make_pidfile builds the correct execution string' do
+      new_pid_file_path = '/path/to/new/pid/file'
+      daemon.make_pid_file(new_pid_file_path)
+      expect(daemon.execution_str).to eq("--make-pidfile=#{new_pid_file_path}")
+    end
+
+    it '#remove_pidfile builds the correct execution string' do
+      pid_file_path = '/path/to/pid/file'
+      daemon.remove_pid_file(pid_file_path)
+      expect(daemon.execution_str).to eq("--remove-pidfile=#{pid_file_path}")
+    end
+
+    it '#verbose builds the correct execution string' do
+      daemon.verbose
+      expect(daemon.execution_str).to eq('--verbose')
+    end
+  end
+end
