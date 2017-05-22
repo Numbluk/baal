98e4978c29fce44c6e9b631117c5e902bf1026ae
Lukas Nimmo <Lukas.nimmo@gmail.com>
2017-05-22 17:11:24 -0500

    Refactored matching options into its own module and separated tests from the Baal module.

--- lib/baal/matching_options.rb
+++ lib/baal/matching_options.rb
@@ -1,5 +1,64 @@
 module Baal
   module MatchingOptions
+    MATCHING_OPTIONS = {
+        pid: '--pid',
+        ppid: '--ppid',
+        pid_file: '--pidfile',
+        exec: '--exec',
+        name: '--name',
+        user: '--user'
+    }.freeze
 
+    def pid(id)
+      @execution_str += "#{MATCHING_OPTIONS[:pid]}=#{id}"
+      self
+    end
+    alias with_pid pid
+
+    def ppid(id)
+      @execution_str += "#{MATCHING_OPTIONS[:ppid]}=#{id}"
+      self
+    end
+    alias with_ppid ppid
+
+    def pid_file(path)
+      @execution_str += "#{MATCHING_OPTIONS[:pid_file]}=#{path}"
+      self
+    end
+    alias with_pid_file pid_file
+    alias pidfile pid_file
+
+    def exec(abs_path_to_executable)
+      @execution_str += "#{MATCHING_OPTIONS[:exec]}=#{abs_path_to_executable}"
+      self
+    end
+    alias instance_of_exec exec
+
+    def name(process_name)
+      @execution_str += "#{MATCHING_OPTIONS[:name]}=#{process_name}"
+      self
+    end
+    alias with_name name
+
+    # TODO: Put big alert in documentation
+    def user(username_or_uid)
+      @execution_str += "#{MATCHING_OPTIONS[:user]}=#{username_or_uid}"
+      self
+    end
+    alias username user
+    alias uid user
+    alias owned_by_user user
+    alias owned_by_username user
+    alias owned_by_uid user
+
+    private
+
+    def at_least_one_matching_option?
+      MATCHING_OPTIONS.each do |_, option|
+        return if @execution_str.include? option
+      end
+
+      raise ArgumentError, 'You must have at least one matching option.'
+    end
   end
 end
---
+++ spec/matching_options_spec.rb
@@ -0,0 +1,58 @@
+require 'rspec'
+require 'baal/matching_options'
+
+class DummyDaemon
+  include Baal::MatchingOptions
+
+  attr_reader :execution_str
+
+  def initialize
+    @execution_str = ''
+  end
+
+  def daemonize!
+    at_least_one_matching_option?
+  end
+end
+
+describe Baal::MatchingOptions do
+  let(:daemon) { DummyDaemon.new }
+
+  it "raises an error if at least one matching option isn't executed" do
+    expect { daemon.daemonize! }.to raise_error(ArgumentError)
+  end
+
+  context 'when building the execution string using a command using a matching option' do
+    it '#pid builds the correct execution string' do
+      daemon.pid(1234)
+      expect(daemon.execution_str).to eq('--pid=1234')
+    end
+
+    it '#ppid builds the correct execution string' do
+      daemon.ppid(1234)
+      expect(daemon.execution_str).to eq('--ppid=1234')
+    end
+
+    it '#pid_file builds the correct execution string' do
+      pid_file_path = '/what/a/great/path'
+      daemon.pid_file(pid_file_path)
+      expect(daemon.execution_str).to eq("--pidfile=#{pid_file_path}")
+    end
+
+    it '#exec builds the correct execution string' do
+      abs_path_to_executable = '/abs/path/to/executable'
+      daemon.exec(abs_path_to_executable)
+      expect(daemon.execution_str).to eq("--exec=#{abs_path_to_executable}")
+    end
+
+    it '#name builds the correct execution string' do
+      daemon.name('great_process')
+      expect(daemon.execution_str).to eq('--name=great_process')
+    end
+
+    it '#user builds the correct execution string' do
+      daemon.user('dave')
+      expect(daemon.execution_str).to eq('--user=dave')
+    end
+  end
+end
