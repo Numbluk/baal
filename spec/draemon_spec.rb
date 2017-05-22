6bf86c7104816d978204ee544dc79d5016079e0c
Lukas Nimmo <Lukas.nimmo@gmail.com>
2017-05-22 12:35:04 -0500

    Moved the checking of the proper commands and options closer to the beginning of tests.

--- spec/draemon_spec.rb
+++ spec/draemon_spec.rb
@@ -7,14 +7,6 @@ describe Draemon do
     expect(Draemon::VERSION).not_to be nil
   end
 
-  it 'raises an error if no commands are executed' do
-    expect { draemon.daemonize! }.to raise_error(ArgumentError)
-  end
-
-  it 'raises an error if more than one command is executed' do
-    expect { draemon.start.stop }.to raise_error(ArgumentError)
-  end
-
   context 'start-stop-daemon --help output' do
     let(:cli_help_output) { `start-stop-daemon --help` }
 
@@ -24,8 +16,16 @@ describe Draemon do
 
     it 'has all the same commands and options' do
       opts = Draemon::COMMANDS.values + Draemon::MATCHING_OPTIONS.values +
-             Draemon::OPTIONAL_OPTS.values
+          Draemon::OPTIONAL_OPTS.values
       expect(output_parser(cli_help_output).sort) == opts.sort
     end
+  end
+
+  it 'raises an error if no commands are executed' do
+    expect { draemon.daemonize! }.to raise_error(ArgumentError)
+  end
+
+  it 'raises an error if more than one command is executed' do
+    expect { draemon.start.stop }.to raise_error(ArgumentError)
   end
 end
