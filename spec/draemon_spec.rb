9bfad71550bcda748f57cee0f3c321d502054b95
Lukas Nimmo <Lukas.nimmo@gmail.com>
2017-05-22 14:23:47 -0500

    Added testing for optional options for building of the execution string.

--- spec/draemon_spec.rb
+++ spec/draemon_spec.rb
@@ -104,5 +104,106 @@ describe Draemon do
     end
   end
 
-  context 'optional options create the correct output string'
+  context 'optional options create the correct output string' do
+    let(:draemon) { Draemon.new }
+
+    it '#group builds the correct execution string' do
+      draemon.group('public')
+      expect(draemon.execution_str).to eq('--group=public')
+    end
+
+    it '#signal builds the correct execution string' do
+      draemon.signal('SIGINT')
+      expect(draemon.execution_str).to eq('--signal=SIGINT')
+    end
+
+    it '#retry builds the correct execution string' do
+      draemon.retry(5)
+      expect(draemon.execution_str).to eq('--retry=5')
+    end
+
+    it '#start_as builds the correct execution string' do
+      start_as_path = '/start/as/path'
+      draemon.start_as(start_as_path)
+      expect(draemon.execution_str).to eq("--startas=#{start_as_path}")
+    end
+
+    it '#test builds the correct execution string' do
+      draemon.test
+      expect(draemon.execution_str).to eq('--test')
+    end
+
+    it '#oknodo builds the correct execution string' do
+      draemon.oknodo
+      expect(draemon.execution_str).to eq('--oknodo')
+    end
+
+    it '#quiet builds the correct execution string' do
+      draemon.quiet
+      expect(draemon.execution_str).to eq('--quiet')
+    end
+
+    it '#chuid builds the correct execution string' do
+      draemon.chuid(34)
+      expect(draemon.execution_str).to eq('--chuid=34')
+    end
+
+    it '#chroot builds the correct execution string' do
+      draemon.chroot('/bin')
+      expect(draemon.execution_str).to eq('--chroot=/bin')
+    end
+
+    it '#chdir builds the correct execution string' do
+      chdir_path = '../new/path'
+      draemon.chdir(chdir_path)
+      expect(draemon.execution_str).to eq("--chdir=#{chdir_path}")
+    end
+
+    it '#background builds the correct execution string' do
+      draemon.background
+      expect(draemon.execution_str).to eq('--background')
+    end
+
+    it '#no_close builds the correct execution string' do
+      draemon.no_close
+      expect(draemon.execution_str).to eq('--no-close')
+    end
+
+    it '#nice_level builds the correct execution string' do
+      draemon.nice_level(3)
+      expect(draemon.execution_str).to eq('--nicelevel=3')
+    end
+
+    it '#proc_sched builds the correct execution string' do
+      draemon.proc_sched('fifo', 2)
+      expect(draemon.execution_str).to eq('--procsched=fifo:2')
+    end
+
+    it '#io_sched builds the correct execution string' do
+      draemon.io_sched('best-effort', 20)
+      expect(draemon.execution_str).to eq('--iosched=best-effort:20')
+    end
+
+    it '#umask builds the correct execution string' do
+      draemon.umask(777)
+      expect(draemon.execution_str).to eq('--umask=777')
+    end
+
+    it '#make_pidfile builds the correct execution string' do
+      new_pid_file_path = '/path/to/new/pid/file'
+      draemon.make_pid_file(new_pid_file_path)
+      expect(draemon.execution_str).to eq("--make-pidfile=#{new_pid_file_path}")
+    end
+
+    it '#remove_pidfile builds the correct execution string' do
+      pid_file_path = '/path/to/pid/file'
+      draemon.remove_pid_file(pid_file_path)
+      expect(draemon.execution_str).to eq("--remove-pidfile=#{pid_file_path}")
+    end
+
+    it '#verbose builds the correct execution string' do
+      draemon.verbose
+      expect(draemon.execution_str).to eq('--verbose')
+    end
+  end
 end
