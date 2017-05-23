b9c7417d38e25b8d6969bd33c6057f81883a6b4b
Lukas Nimmo <Lukas.nimmo@gmail.com>
2017-05-22 19:13:10 -0500

    Moved the comparison of start-stop-daemon --help output to all commands in Baal from daemon_spec to baal_spec

--- spec/baal_spec.rb
+++ spec/baal_spec.rb
@@ -1,6 +1,24 @@
 require 'spec_helper'
 
 describe Baal do
+  context 'start-stop-daemon --help output' do
+    let(:cli_help_output) { `start-stop-daemon --help` }
+
+    def commands_and_opts_parser(str)
+      commands_and_opts = str.scan(/(--[\w\-]+ )/).flatten.sort
+      commands_and_opts.map(&:strip)
+    end
+
+    it 'has all the same commands and options as baal' do
+      opts = Baal::Daemon::COMMANDS.values +
+             Baal::Daemon::MATCHING_OPTIONS.values +
+             Baal::Daemon::OPTIONAL_OPTS.values
+
+      cli_output_commands_and_opts = commands_and_opts_parser(cli_help_output)
+      expect(opts.sort).to eq(cli_output_commands_and_opts)
+    end
+  end
+
   it 'has a version number' do
     expect(Baal::VERSION).not_to be nil
   end
