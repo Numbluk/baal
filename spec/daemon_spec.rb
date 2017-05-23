80e65fdcd58273632f345bda4c1622fe76cdab64
Lukas Nimmo <Lukas.nimmo@gmail.com>
2017-05-23 16:50:55 -0500

    Added functional test to test #execution interaction with the 3 different command and options moduls

--- spec/daemon_spec.rb
+++ spec/daemon_spec.rb
@@ -2,9 +2,21 @@ require 'rspec'
 require 'baal'
 
 describe Baal::Daemon do
-  it '#clear_all! clears everything except the program name' do
-    daemon = Baal::Daemon.new
-    daemon.start.clear_all!
-    expect(daemon.execution).to eq(Baal::Daemon::PROGRAM_NAME)
+  context '#clear_all!' do
+    it 'clears everything in @executable except the program name' do
+      daemon = Baal::Daemon.new
+      daemon.start.clear_all!
+      expect(daemon.execution).to eq(Baal::Daemon::PROGRAM_NAME)
+    end
+  end
+
+  context '#executable' do
+    it 'can return correctly a correctly spaced string with methods from
+        different modules' do
+      daemon = Baal::Daemon.new
+      daemon.start.with_pid(334).in_background
+      correct_string = 'start-stop-daemon --start --pid=334 --background'
+      expect(daemon.execution).to eq(correct_string)
+    end
   end
 end
